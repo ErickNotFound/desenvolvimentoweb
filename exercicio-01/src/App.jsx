7ef97428da498b883cf8c14d5202d9e640e149db
Erick Viana <erickvianar15@gmail.com>
2022-10-27 22:00:41 -0300

    Att

--- exercicio-01/src/App.jsx
+++ exercicio-01/src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header'
-import Bio from './components/Bio'
-import Footer from './components/Footer'
+// import { useState } from 'react'
+// import './App.css'
+// import Header from './components/Header'
+// import Bio from './components/Bio'
+// import Footer from './components/Footer'
 
 
-function App() {
+// function App() {
 
 
-  return (
-    <>
-      <Header />
-      <Bio texto="Saiba mais" />
-      <Footer />
-    </>
-  )
+//   return (
+//     <>
+//       <Header />
+//       <Bio texto="Saiba mais" />
+//       <Footer />
+//     </>
+//   )
+// }
+
+// export default App
+
+let nome = "Erick"
+function App(){
+    return(
+      <h1>{nome}</h1>
+    )
 }
 
 export default App
